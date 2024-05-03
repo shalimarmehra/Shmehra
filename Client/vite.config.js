2b7e9429a0e3a14f40dfc1d284d405a0de355c68
shalimar-mehra <shalimarmehraa@gmail.com>
2024-05-03 13:02:50 +0530

    ok

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
-      "/client": {
+      "/api": {
         target: "http://localhost:3000",
         secure: false,
       },
